925c5b0cefd332cc5b5e31d8facacc2a59af856b
Dzhalsan <erdniev08jalsan@gmail.com>
2023-09-29 19:26:10 -0400

    All HW

--- HomeAssignment1/src/Day17A2.java
+++ HomeAssignment1/src/Day17A2.java
@@ -1,53 +1,94 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Day17A2 {
     public static void main(String[] args) {
 
-//        1. Write a program which is about elevator in
-//        a residential building with 25 floors, u can go to any floors,
-//                user is gonna floors to go, one he wishes to stop while loop needs to break.
-//
-//* 15th and 23rd floor are under construction, if one of them is selected, then while loop should stop
-//                * if entered floor is wrong like greater than 25 or less than 0, then while loop should stop
-//                * Every time before selecting the floor, user must be asked he/she wants to continue yes or no
-//
-//        You must create this variables int currentFloor outside of the loop,
-//        and floorToSelect and String answer inside loop.
-//                And you must be able to change your floors, and the while loop stops
-//        you must be able to see number how many times has the elevator moved
+        // task 1
 
         Scanner scanner = new Scanner(System.in);
-        short[] floors = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-        short currentFloor = 1;
-        int counter =0;
-        String word = "yes";
-        System.out.println("You are on the " + currentFloor + "st(th) floor");
-        System.out.println("Select the flooor you want to go to");
-        currentFloor = scanner.nextShort();
-        scanner.nextLine();
-        if (currentFloor==15 || currentFloor == 23) {
-            System.out.println("Under construction");
-            word = "no";
-        }
+//        short currentFloor = 1;
+//        int counter = 0;
+//        String word = "yes";
+//
+//        do {
+//            System.out.println("You are on the " + currentFloor + "st(th) floor");
+//            if (currentFloor==1) System.out.println("Select the flooor you want to go to");
+//             else {System.out.println("Do you wanna continue?");
+//            word = scanner.next();
+//             if (word.equalsIgnoreCase("no")) break;}
+//            short tempFloor = currentFloor;
+//            currentFloor = scanner.nextShort();
+//            if (currentFloor==15 || currentFloor == 23) {
+//                System.out.println("Under construction");
+//                currentFloor = 1;
+//                word = "no";}
+//            if (0>=currentFloor || currentFloor>25 ) {
+//                System.out.println("Ivalid floor");
+//                currentFloor = 1;
+//            }
+//            if (currentFloor-tempFloor!=0) counter++;
+//
+//
+//        } while (word.equalsIgnoreCase("yes"));
+//        System.out.println("Elevator moved " + counter + " times");
 
+        // task 2
 
+        Random random = new Random();
+        int compWins = 0;
+        int userWins = 0;
+        for (int i = 1 ; i<=5 ; i++ ) {
+            int computerN = random.nextInt(1, 4);
+            System.out.println("Round " + i);
+            System.out.print("Enter your choice (rock, paper, scissors): ");
+            String word = scanner.nextLine();
+            if (word.equalsIgnoreCase("rock") || word.equalsIgnoreCase("paper") || word.equalsIgnoreCase("scissors")){
+                if (computerN == 1 && word.equalsIgnoreCase("rock")){
+                    System.out.println("Computer chooses " + word);
+                    System.out.println("Tie");}
+                else if (computerN == 1 && word.equalsIgnoreCase("paper")){
+                    System.out.println("Computer chooses " + word);
+                    userWins++;
+                    System.out.println("You won this round");}
+                else if (computerN == 1 && word.equalsIgnoreCase("scissors")){
+                    System.out.println("Computer chooses " + word);
+                    compWins++;
+                    System.out.println("Computer wins this round!");}
+                else if (computerN == 2 && word.equalsIgnoreCase("paper")){
+                    System.out.println("Computer chooses " + word);
+                    System.out.println("Tie");}
+                else if (computerN == 2 && word.equalsIgnoreCase("scissors")){
+                    System.out.println("Computer chooses " + word);
+                    userWins++;
+                    System.out.println("You won this round");}
+                else if (computerN == 2 && word.equalsIgnoreCase("rock")){
+                    System.out.println("Computer chooses " + word);
+                    compWins++;
+                    System.out.println("Computer wins this round");}
+                else if (computerN == 3 && word.equalsIgnoreCase("scissors")){
+                    System.out.println("Computer chooses " + word);
+                    System.out.println("Tie");}
+                else if (computerN == 3 && word.equalsIgnoreCase("paper")){
+                    System.out.println("Computer chooses " + word);
+                    userWins++;
+                    System.out.println("You won this round");}
+                else if (computerN == 3 && word.equalsIgnoreCase("rock")){
+                    System.out.println("Computer chooses " + word);
+                    compWins++;
+                    System.out.println("Computer wins this round!");}
 
-        while (word.equalsIgnoreCase("yes"))
-        {
-            counter++;
-            System.out.println("You are on the " + currentFloor + "st(th) floor");
-            System.out.println("Do you want to continue?");
-            word = scanner.nextLine();
-            scanner.next();
-            currentFloor = scanner.nextShort();
-            if (currentFloor==15 || currentFloor == 23) {
-                System.out.println("Under construction");
-                break;
             }
-
-
-
+            else {
+                System.out.println("Invalid input");
+                i--;
+            }
         }
-        System.out.println("Elevator moved " + counter + " times");
+        System.out.println("Game over!");
+        System.out.println("User wins: " + userWins);
+        System.out.println("Computer wins: " + compWins);
+
+
+
     }
 }
