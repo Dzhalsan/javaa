d6d950f890c03bced23a142e660003162f4d7f87
Dzhalsan <erdniev08jalsan@gmail.com>
2023-09-29 20:33:44 -0400

    class

---
+++ Classroom/src/Day19/Classroom1.java
@@ -0,0 +1,8 @@
+package Day19;
+
+public class Classroom1 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Classroom/src/Day19/Practice.java
@@ -0,0 +1,12 @@
+package Day19;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        Scanner scanner = new Scanner()
+        System.out.println(git );
+
+    }
+}
